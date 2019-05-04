c3564e9fb522219e19d6707b9fa7372da17e9c60
AnnaNavarroM <42281858+AnnaNavarroM@users.noreply.github.com>
2019-05-04 12:29:38 -0300

    Update ping.js

--- commands/miscellaneous/ping.js
+++ commands/miscellaneous/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "ping",
         description: "PONG! Displays the api & bot latency",
-        usage: "!ping",
+        usage: "?ping",
         category: "miscellaneous",
         accessableby: "Members"
     },
@@ -10,7 +10,7 @@ module.exports = {
 
     message.channel.send("Pinging...").then(m => {
         let ping = m.createdTimestamp - message.createdTimestamp
-        let choices = ["Is this really my ping?", "Is it okay? I can't look!", "I hope it isn't bad!"]
+        let choices = ["Is this really my ping?", "Is it okay?", "I hope it isn't bad!"]
         let response = choices[Math.floor(Math.random() * choices.length)]
 
         m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
