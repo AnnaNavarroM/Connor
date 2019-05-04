154fe032e8cb9dda14d706a052464d9e0b376391
AnnaNavarroM <42281858+AnnaNavarroM@users.noreply.github.com>
2019-05-04 13:56:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const keepalive = require('express-glitch-keepalive');
+
+const app = express();
+
+app.use(keepalive);
+
+app.get('/', (req, res) => {
+res.json('This bot should be online! Uptimerobot will keep it alive');
+});
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
 const { Client, Collection } = require("discord.js");
 const { token } = require("./botconfig.json");
 const { RichEmbed } = require("discord.js")
